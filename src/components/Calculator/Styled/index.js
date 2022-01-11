fa1ab6cc5d4680696b3cbd671e5c3057eb79053d
Tsai-Chieh <>
2022-01-11 17:51:15 +0800

    :sparkles: add func in Button

--- src/components/Calculator/Styled/index.js
+++ src/components/Calculator/Styled/index.js
@@ -45,10 +45,10 @@ export const Button = styled.button`
   &:hover {
     background-color: rgba(255, 255, 255, .9);
   }
-  /* EXPANDED FROM EXAMPLE */
-  ${ function({ gridSpan }) {
-      if (gridSpan) {
-        return `grid-column: span ${gridSpan}`
-      }
-  } }
+  
+  ${({ gridSpan }) => gridSpan && `grid-column: span ${gridSpan};`}
+  ${({ operation }) => operation && `background-color: gray;`}
+  ${({ control }) => control && `background-color: skyblue;`}
+  ${({ equals }) => equals && `border-bottom-right-radius: 10px; background-color: gray;`}
+  ${({ period }) => period && `border-bottom-left-radius: 10px; background-color: skyblue;`}
 `
